e4035b24f040b3b1b5810d01a686f33ede27b21f
Juha Hellen <juha.hellen@aalto.fi>
2020-11-29 21:51:21 +0200

    mod: refactoring

--- inc/grayvalley/smd/SMDClient.hh
+++ inc/grayvalley/smd/SMDClient.hh
@@ -3,7 +3,6 @@
 #include <string>
 #include <functional>
 #include <grayvalley/core/epolling.hh>
-#include <grayvalley/core/fragment.hh>
 #include <grayvalley/smd/SMD.hh>
 #include <grayvalley/smd/SMDParser.hh>
 #include <grayvalley/core/RFC6455.hh>
@@ -12,28 +11,26 @@ namespace RFC6455{
     class Frame;
 }
 
-namespace GVT {
-    namespace SMD {
-        class SMDClient : public Epollable {
-        private:
-            RFC6455::Session m_session;
-            SMDParser* m_p_parser;
-        public:
-            SMDClient() = delete;
-            PREVENT_COPY(SMDClient);
-        public:
-            explicit SMDClient(SMDParser* p_parser);
-            ~SMDClient();
-        private:
-            void recv(Epoller* p_epoller) override;
-            void read();
-            void read_handshake_response();
-            void read_websocket_frame();
-            void send(const FragmentView& fragment);
-        public:
-            void connect(const RFC6455::Session& session);
-            void open();
-        };
-    }
+namespace GVT::SMD {
+    class SMDClient : public Epollable {
+    private:
+        RFC6455::Session m_session;
+        SMDParser* m_p_parser;
+    public:
+        SMDClient() = delete;
+        PREVENT_COPY(SMDClient);
+    public:
+        explicit SMDClient(SMDParser* p_parser);
+        ~SMDClient();
+    private:
+        void recv(Epoller* p_epoller) override;
+        void read();
+        void read_handshake_response();
+        void read_websocket_frame();
+        void send(const StringView& fragment);
+    public:
+        void connect(const RFC6455::Session& session);
+        void open();
+    };
 }
 #endif //SMDCLIENT_HH
